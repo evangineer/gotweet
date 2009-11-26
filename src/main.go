22939f11229ad3b9f7045783aa64b7871b3deeb0
evangineer <mamading@gmail.com>
2009-11-26 03:59:27 +0000

    added support for sent direct messages & modified copyright notices

--- src/main.go
+++ src/main.go
@@ -1,4 +1,5 @@
 // Copyright 2009 Dmitry Chestnykh <dmitry@codingrobots.com>
+// Modified 2009 Mamading Ceesay <mamading@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
